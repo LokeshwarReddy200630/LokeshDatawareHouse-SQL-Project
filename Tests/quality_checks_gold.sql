9adee5ab37dd0584a4a0ef646323abc53e766e4b
LokeshwarReddy200630 <lavanurulokeshwarreddy@gmail.com>
2026-09-23 01:13:16 +0530

    Add quality checks for Gold Layer data integrity
    This SQL script performs quality checks on the Gold Layer, ensuring data integrity and consistency by checking for duplicate surrogate keys and referential integrity between tables.

---
+++ Tests/quality_checks_gold.sql
@@ -0,0 +1,46 @@
+/*
+===============================================================================
+Quality Checks
+===============================================================================
+Code Purpose:
+    This code performs quality checks to validate the integrity, consistency, 
+    and accuracy of the Gold Layer. These checks ensure:
+    - Uniqueness of surrogate keys in dimension tables.
+    - Referential integrity between fact and dimension tables.
+    - Validation of relationships in the data model for analytical purposes.
+
+Usage Notes:
+    - Investigate and resolve any discrepancies found during the checks.
+===============================================================================
+*/
+
+-- ====================================================================
+-- Checking 'gold.customers'
+-- ====================================================================
+SELECT 
+    customer_key,
+    COUNT(*) AS duplicate_count
+FROM gold.customers
+GROUP BY customer_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.product_key'
+-- ====================================================================
+SELECT 
+    product_key,
+    COUNT(*) AS duplicate_count
+FROM gold.products
+GROUP BY product_key
+HAVING COUNT(*) > 1;
+
+-- ====================================================================
+-- Checking 'gold.sales'
+-- ====================================================================
+SELECT * 
+FROM gold.sales f
+LEFT JOIN gold.customers c
+ON c.customer_key = f.customer_key
+LEFT JOIN gold.products p
+ON p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL  
